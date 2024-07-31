599763841e7f61e4a3ce1a2915ac9798ed7ef867
Vicente Lyon <vlyon@uc.cl>
2024-07-31 12:36:16 -0400

    movies list

--- owen-wilson-app/src/components/Movies.jsx
+++ owen-wilson-app/src/components/Movies.jsx
@@ -1,11 +1,40 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 function Movies() {
     const [movies, setMovies] = useState([]);
+    const [sorted, setSorted] = useState(false);
+
+    useEffect(() => {
+        fetch("https://owen-wilson-wow-api.onrender.com/wows/movies")
+            .then((response) => response.json())
+            .then((data) => setMovies(data));
+    }
+    , []);
+
+    const sortMovies = () => {
+        const sortedMovies = [...movies].sort();
+        setMovies(sortedMovies);
+        setSorted(true);
+    };
+
+    const navigate = useNavigate();
+
+    const habdleBackClick = () => {
+        navigate('/');
+    };
     
     return (
         <div className='movies'>
-            <h1>Lista de películas en las que Owen Wilson dice "Wow"</h1>
+            <h2>Lista de películas en las que Owen Wilson dice "Wow"</h2>
+            <button onClick={sortMovies}>Ordenar películas</button>
+            <ul>
+                {movies.map((movie, index) => (
+                    <li key={index}>{movie}</li>
+                ))}
+            </ul>
+            <button onClick={habdleBackClick}>Volver</button>
         </div>
     );
 }
